0595fda2dfb3f48618db77651462c1b7b9ceb84a
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2021-05-19 17:38:17 +0200

    Camera usage

--- app/src/main/java/com/brightnetwork/summerfests/MainActivity.kt
+++ app/src/main/java/com/brightnetwork/summerfests/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.brightnetwork.summerfests
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
 
     var text = ""
+    lateinit var imageToReplace : ImageView
+
+    val getPictureFromCameraAction = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            val image = result.data?.extras?.get("data") as Bitmap
+            imageToReplace.setImageBitmap(image)
+        }
+    }
 
     companion object {
         const val textKey = "TEXT_KEY"
@@ -21,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         Log.d("LIFECYCLE", "onCreate")
         Log.d("FESTFEST-0", "InputText:${text}")
         setContentView(R.layout.activity_main)
+        imageToReplace = findViewById(R.id.imageView)
         val input = findViewById<EditText>(R.id.searchFestivalInputField)
 
         val searchButton = findViewById<Button>(R.id.searchForFestivals)
@@ -29,6 +44,14 @@ class MainActivity : AppCompatActivity() {
             Log.d("FESTFEST-1", "InputText:${text}: $this")
             val festivals = Datasource().loadFestivals(this)
             Log.d("FESTFEST-2", "Festivals:${festivals}")
+        }
+
+        setupCameraEntryPoint()
+    }
+
+    private fun setupCameraEntryPoint() {
+        imageToReplace.setOnClickListener {
+            getPictureFromCameraAction.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
         }
     }
 
