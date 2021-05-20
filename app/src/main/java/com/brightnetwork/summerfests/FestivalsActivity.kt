109feacb7df67703bc2519a18b3589d9059c4d9f
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2021-05-20 11:58:47 +0200

    Add retrofit

--- app/src/main/java/com/brightnetwork/summerfests/FestivalsActivity.kt
+++ app/src/main/java/com/brightnetwork/summerfests/FestivalsActivity.kt
@@ -3,8 +3,12 @@ package com.brightnetwork.summerfests
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class FestivalsActivity : AppCompatActivity() {
 
@@ -19,5 +23,20 @@ class FestivalsActivity : AppCompatActivity() {
                     }
             }
         }
+        fetchFestivals()
+    }
+
+    private fun fetchFestivals() {
+        val call = NetworkService.festivalService.getFestivals()
+        call.enqueue(object : Callback<List<FestivalDTO>> {
+            override fun onResponse(call: Call<List<FestivalDTO>>, response: Response<List<FestivalDTO>>) {
+                Log.d("RETROFIT", "Response: ${response.body()}")
+            }
+
+            override fun onFailure(call: Call<List<FestivalDTO>>, t: Throwable) {
+                Log.e("RETROFIT", "Failed to fetch festivals", t)
+            }
+
+        })
     }
 }
